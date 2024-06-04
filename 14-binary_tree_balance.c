866223d71091630cae410aba16698e11095b0afd
Collen <damd819@gmail.com>
2024-06-04 18:12:05 +0200

    bugs

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,15 +11,10 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL && tree->right == NULL)
-	{
-		return (binary_tree_balance(tree->left)
-		- binary_tree_balance(tree->right) + 1);
-	}
+		return (binary_tree_balance(tree->left) + 1);
 	else if (tree->right != NULL && tree->left == NULL)
-	{
-		return (binary_tree_balance(tree->left)
-		- binary_tree_balance(tree->right) - 1);
-	}
+		return (binary_tree_balance(tree->right) - 1);
+		
 	return (binary_tree_balance(tree->left) - binary_tree_balance(tree->right));
 
 }
