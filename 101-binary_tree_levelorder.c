43fd28a536adcdf9aadaa97a8eeeb91c04a1bbfe
Collen <damd819@gmail.com>
2024-06-08 01:32:12 +0200

    bst remove

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,35 +1,118 @@
 #include "binary_trees.h"
-#define MAX_QUEUE_SIZE 100
 
 /**
- * binary_tree_levelorder -  function that goes through a binary tree
- *						 using level-order traversal.
- * @tree: root node of tree to traverse.
- * @func: pointer to a function to call for each node. The value in the
- *		node must be passed as a parameter to this function.
-*/
+ * bst_stack_push - Pushes a node onto the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @node: The binary tree node to push onto the stack.
+ *
+ * Return: A pointer to the new top of the stack.
+ */
+bst_t *bst_stack_push(bst_t **stack, binary_tree_t *node)
+{
+	bst_t *new;
 
+	new = malloc(sizeof(bst_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = (int)node; 
+	new->left = NULL;
+	new->right = *stack;
+	if (*stack != NULL)
+		(*stack)->parent = new;
+	new->parent = NULL;
+	*stack = new;
+
+	return (new);
+}
+
+/**
+ * bst_stack_pop - Pops the top node off the stack.
+ * @stack: Double pointer to the top of the stack.
+ *
+ * Return: A pointer to the binary tree node popped off the stack.
+ */
+binary_tree_t *bst_stack_pop(bst_t **stack)
+{
+	bst_t *top;
+	binary_tree_t *node;
+
+	if (*stack == NULL)
+		return (NULL);
+
+	top = *stack;
+	node = (binary_tree_t *)top->n;
+	*stack = top->right;
+	if (*stack != NULL)
+		(*stack)->parent = NULL;
+	free(top);
+
+	return (node);
+}
+
+/**
+ * free_stack - Frees all nodes in the stack.
+ * @stack: Pointer to the top of the stack.
+ */
+void free_stack(bst_t *stack)
+{
+	bst_t *tmp;
+
+	while (stack != NULL)
+	{
+		tmp = stack->right;
+		free(stack);
+		stack = tmp;
+	}
+}
+
+/**
+ * binary_tree_levelorder - Traverses a binary tree using
+ *						  level-order traversal.
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node.
+ */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
+	bst_t *stack = NULL, *tail = NULL;
+	binary_tree_t *node;
+
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_t *queue[MAX_QUEUE_SIZE];
-	int front = 0;
-	int rear = 0;
+	if (bst_stack_push(&stack, (binary_tree_t *)tree) == NULL)
+		return;
 
-	queue[rear++] = (binary_tree_t *)tree;
-
-	while (front < rear)
+	while (stack != NULL)
 	{
-		binary_tree_t *current = queue[front++];
+		node = bst_stack_pop(&stack);
+		func(node->n);
 
-		func(current->n);
+		if (node->left != NULL)
+		{
+			if (bst_stack_push(&tail, node->left) == NULL)
+			{
+				free_stack(stack);
+				return;
+			}
+		}
+		if (node->right != NULL)
+		{
+			if (bst_stack_push(&tail, node->right) == NULL)
+			{
+				free_stack(stack);
+				return;
+			}
+		}
 
-		if (current->left != NULL)
-			queue[rear++] = current->left;
-
-		if (current->right != NULL)
-			queue[rear++] = current->right;
+		if (stack == NULL)
+			stack = tail;
+		else
+		{
+			while (stack->right != NULL)
+				stack = stack->right;
+			stack->right = tail;
+		}
+		tail = NULL;
 	}
 }
